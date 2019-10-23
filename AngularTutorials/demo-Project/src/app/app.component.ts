d153ee4fca5fbcef5c94c9e2d9eaf486a11199a8
unknown <sagar536.chinnu@gmail.com>
2019-10-23 20:02:33 +0530

    update1

--- AngularTutorials/demo-Project/src/app/app.component.ts
+++ AngularTutorials/demo-Project/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo-Project';
+  loadPage:string = "recipe";
+
+  onNavigate(option:string)
+  {
+    this.loadPage = option;
+  }
 }
