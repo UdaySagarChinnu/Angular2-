02e9575f50fc3863a13e2d8c99a68593c3dca1d4
unknown <sagar536.chinnu@gmail.com>
2019-11-13 20:38:17 +0530

    directive Changes

--- AngularTutorials/demo/src/app/directives/basichighlightdirective.directive.ts
+++ AngularTutorials/demo/src/app/directives/basichighlightdirective.directive.ts
@@ -1,10 +1,31 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener, HostBinding, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appBasichighlightdirective]'
 })
-export class BasichighlightdirectiveDirective {
+export class BasichighlightdirectiveDirective implements OnInit {
 
-  constructor() { }
+@Input() defaultBg:string="transperant";
+@Input() downlighted:string;
+@Input() highlight:string;
+
+
+@HostBinding("style.backgroundColor") backgroundColor:string="lightgreen";
+  constructor(private eleRef : ElementRef,private reference : Renderer2) { }
+
+ngOnInit(){
+  this.backgroundColor= this.defaultBg;
+}
+ @HostListener('mouseenter') onMouseOver()
+  {
+    //this.reference.setStyle(this.eleRef.nativeElement,"backgroundColor","lightblue");
+    this.backgroundColor=this.highlight;
+  }
+
+  @HostListener('mouseleave') onMouseOut()
+  {
+    //this.reference.setStyle(this.eleRef.nativeElement,"backgroundColor","lightgreen");
+    this.backgroundColor= this.downlighted;
+  }
 
 }
