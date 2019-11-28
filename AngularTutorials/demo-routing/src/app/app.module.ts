af17d629b3f8e2b06eb9f06c04f4eb3638b19e92
unknown <sagar536.chinnu@gmail.com>
2019-11-28 23:12:27 +0530

    initial Routing

---
+++ AngularTutorials/demo-routing/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Routes,RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './Users/user/user.component';
+import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './Servers/server/server.component';
+
+const appRoutes:Routes=[
+  {path:'', component:HomeComponent},
+  {path:'Users', component:UsersComponent},
+  {path:'Servers', component:ServersComponent},
+  {path:'User', component:UserComponent}
+]
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    HomeComponent,
+    ServersComponent,
+    ServerComponent
+  ],
+  imports: [
+    BrowserModule,FormsModule,RouterModule.forRoot(appRoutes),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
